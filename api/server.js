0de7650737212bf157534b7294717c0b9c2037d3
msi1995 <lloyddo@oregonstate.edu>
2023-07-03 01:13:31 -0700

    sanity

--- api/server.js
+++ api/server.js
@@ -53,7 +53,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build'));
   });
 
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build'));
   });
 
