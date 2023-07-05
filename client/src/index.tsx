42052b97acf7f511b1aa16aeefe2ced5552eb4b6
msi1995 <lloyddo@oregonstate.edu>
2023-07-04 18:56:26 -0700

    google analytics tracking id

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from "react-ga4"
 
+
+ReactGA.initialize("G-TBQ58GH21Z");
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
