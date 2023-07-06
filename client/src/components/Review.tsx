daa8a7248cdae402c88b47dd7253c9d0ec470a68
msi1995 <lloyddo@oregonstate.edu>
2023-07-05 18:11:12 -0700

    review success flow

--- client/src/components/Review.tsx
+++ client/src/components/Review.tsx
@@ -9,6 +9,7 @@ export const Review = ({ setReviewScreen }: any) => {
   const [email, setEmail] = useState<string>("");
   const [city, setCity] = useState<string>("");
   const [reviewBody, setReviewBody] = useState<string>("");
+  const [reviewSubmitted, setReviewSubmitted] = useState<boolean>(false);
 
   const handleReviewSubmit = async (event: any) => {
     event.preventDefault();
@@ -30,9 +31,14 @@ export const Review = ({ setReviewScreen }: any) => {
         email: email,
       }),
     });
-    emailjs.send("service_njnh1t8", "template_1174msd", undefined, "jH27kSqIiUkbPafAF");
+    emailjs.send(
+      "service_njnh1t8",
+      "template_1174msd",
+      undefined,
+      "jH27kSqIiUkbPafAF"
+    );
 
-    setReviewScreen(false);
+    setReviewSubmitted(true);
   };
 
   return (
@@ -49,133 +55,149 @@ export const Review = ({ setReviewScreen }: any) => {
           }}
         />
       </div>
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Add your review
-        </h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Fill out your review below!
-        </p>
-      </div>
-      <form
-        onSubmit={handleReviewSubmit}
-        className="mx-auto mt-16 max-w-xl sm:mt-20"
-      >
-        <div className="grid grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2">
-          <div>
-            <label
-              htmlFor="first-name"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              First name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                name="first_name"
-                id="first-name"
-                autoComplete="given-name"
-                className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              htmlFor="last-name"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Last Initial
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                value={lastInitial}
-                onChange={(e) => setLastInitial(e.target.value)}
-                name="last_name"
-                id="last-name"
-                maxLength={1}
-                autoComplete="family-name"
-                className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                name="email"
-                id="email"
-                autoComplete="email"
-                className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="city"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              City
-            </label>
-            <div className="mt-2.5">
-              <select
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
-                name="city"
-                id="city"
-                className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              >
-                <option value="Corvallis, OR">Corvallis</option>
-                <option value="Philomath, OR">Philomath</option>
-                <option value="Albany, OR">Albany</option>
-              </select>
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              What did you think?
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                maxLength={350}
-                value={reviewBody}
-                onChange={(e) => setReviewBody(e.target.value)}
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="mt-12 flex flex-row space-x-12">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Submit Review
-          </button>
+      {reviewSubmitted ? (
+        <div className="flex flex-col text-center isolate px-6 py-60 sm:py-24 lg:px-8 sm:py-96">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-2xl">
+            Got it! Your review has been received.
+          </h2>
           <button
             onClick={() => setReviewScreen(false)}
-            className="block w-full rounded-md bg-slate-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="mt-4 mx-auto flex justify-center w-48 rounded-md bg-pink-400 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Nevermind
+            Back to Testimonials
           </button>
         </div>
-      </form>
+      ) : (
+        <>
+          <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Add your review
+            </h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600">
+              Fill out your review below!
+            </p>
+          </div>
+          <form
+            onSubmit={handleReviewSubmit}
+            className="mx-auto z-20 mt-16 max-w-xl sm:mt-20"
+          >
+            <div className="grid grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    name="first_name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Last Initial
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    value={lastInitial}
+                    onChange={(e) => setLastInitial(e.target.value)}
+                    name="last_name"
+                    id="last-name"
+                    maxLength={1}
+                    autoComplete="family-name"
+                    className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="city"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  City
+                </label>
+                <div className="mt-2.5">
+                  <select
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    name="city"
+                    id="city"
+                    className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  >
+                    <option value="Corvallis, OR">Corvallis</option>
+                    <option value="Philomath, OR">Philomath</option>
+                    <option value="Albany, OR">Albany</option>
+                  </select>
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  What did you think?
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    maxLength={350}
+                    value={reviewBody}
+                    onChange={(e) => setReviewBody(e.target.value)}
+                    name="message"
+                    id="message"
+                    rows={4}
+                    className="block w-full sm:border-0 border-solid border-2 border-slate-500 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 flex flex-row space-x-12">
+              <button
+                type="submit"
+                className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Submit Review
+              </button>
+              <button
+                onClick={() => setReviewScreen(false)}
+                className="block w-full rounded-md bg-slate-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Nevermind
+              </button>
+            </div>
+          </form>
+        </>
+      )}
     </div>
   );
 };
