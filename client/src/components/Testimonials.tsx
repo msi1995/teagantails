8cb3e57637d23a21dcda1f5032e787731168e61a
msi1995 <lloyddo@oregonstate.edu>
2023-07-13 01:48:45 -0700

    base route logic

--- client/src/components/Testimonials.tsx
+++ client/src/components/Testimonials.tsx
@@ -5,7 +5,8 @@ import { ReactComponent as TLogoBlack } from "../assets/tgwalk_black.svg";
 import "../animations.css";
 
 // const BASE_ROUTE = "https://teagandfriends-5ef46929a672.herokuapp.com";
-const BASE_ROUTE = "http://localhost:3001";
+// const BASE_ROUTE = "http://localhost:3001";
+const BASE_ROUTE = process.env.MONGODB_URI ? '' : 'http://localhost:3001'
 
 interface Testimonial {
   reviewText: string;
@@ -50,7 +51,7 @@ export const Testimonials = () => {
 
   const getTestimonials = async () => {
     try {
-      const res: Response = await fetch("/reviews/approved");
+      const res: Response = await fetch(BASE_ROUTE + "/reviews/approved");
       const data: Testimonial[] = await res.json();
       setTestimonials(data);
       setIsLoading(false);
