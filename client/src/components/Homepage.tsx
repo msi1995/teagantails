c6d14a90f4349ab7c2042c597a6a97f8be0c0fb0
msi1995 <lloyddo@oregonstate.edu>
2023-08-07 18:08:09 -0700

    h265

--- client/src/components/Homepage.tsx
+++ client/src/components/Homepage.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import "../index.css";
 import "../animations.css";
 // import bg_video from "../assets/Tgnmuted264.mp4";
-import bg_video from "../assets/hd_alternativeWEBM.webm";
+import bg_video from "../assets/hd_trimmed265.mp4";
 
 const heroTextIntro: string = `Hi, I'm Teagan.`;
 const heroTextBase: string = `I `;
